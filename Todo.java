98c749227a082df89d4b61cccc686c27a711a496
arifqi3636 <160752288+arifqi3636@users.noreply.github.com>
2025-04-14 17:47:52 +0700

    Add files via upload

---
+++ Todo.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.peliharaan;
+
+/**
+ *
+ * @author Zulfa Arifqi
+ */
+public class Todo {
+    private int id;
+    private String title;
+    private String description;
+    private boolean isCompleted;
+    private String createdAt;
+
+    public Todo(int id, String title, String description, boolean isCompleted, String createdAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.isCompleted = isCompleted;
+        this.createdAt = createdAt;
+    }
+
+    public Todo(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", isCompleted=" + isCompleted +
+                ", createdAt='" + createdAt + '\'' +
+                '}';
+    }
+}
